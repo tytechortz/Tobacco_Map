82e917b22fd320d51bcf5bf0a8266887eab5b297
James Swank <jmswank7@gmail.com>
2023-12-18 06:58:08 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,132 @@
+import pandas as pd
+import numpy as np
+from dash import Dash, html, dcc, Input, Output, State, ctx, dash_table
+import dash_bootstrap_components as dbc
+from datetime import date
+import geopandas as gpd
+
+
+from figures_utilities import (
+    get_figure
+)
+
+from utilities import (
+    get_grocery_stores,
+    get_block_data
+)
+
+
+
+
+
+app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
+
+header = html.Div("Arapahoe County Tobacco Retailers", className="h2 p-2 text-white bg-primary text-center")
+
+bgcolor = "#f3f3f1"  # mapbox light map land color
+
+template = {"layout": {"paper_bgcolor": bgcolor, "plot_bgcolor": bgcolor}}
+
+theme = {
+    'dark': True,
+    'detail': '#007439',
+    'primary': '#00EA64',
+    'secondary': '#6E6E6E',
+}
+
+def blank_fig(height):
+    """
+    Build blank figure with the requested height
+    """
+    return {
+        "data": [],
+        "layout": {
+            "height": height,
+            "template": template,
+            "xaxis": {"visible": False},
+            "yaxis": {"visible": False},
+        },
+    }
+
+
+
+app.layout = dbc.Container([
+    header,
+    dbc.Row([
+        # dbc.Col([
+        #     dcc.Checklist(
+        #         id="stores",
+        #         options=[
+        #             {"label": i, "value": i}
+        #             for i in ["Safeway", "King Sooper", "Sprouts", "Walmart SC", "Walmart NM", "Whole Foods", "Trader Joe's", "Target", "Save A Lot", "Sams", "Natural Grocers", "Costco", "Lowe's", "El Mercado De Colorado" ]
+        #         ],
+        #         value=["Safeway", "King Sooper", "Sprouts", "Walmart SC", "Walmart NM", "Whole Foods", "Trader Joe's", "Target", "Save A Lot", "Sams", "Natural Grocers", "Costco", "Lowe's", "El Mercado De Colorado" ],
+        #         inline=True
+        #     ),
+        # ], width=6),
+        dbc.Col([
+            dcc.Input(
+                id='buffer',
+                type='number',
+                value=.5,
+                step=.1,
+                placeholder='Input radius in km'
+            )
+        #     dcc.Slider(0, 2, value=1.6,
+        #         marks={
+        #             0: {'label': '0', 'style': {'color': 'white'}},
+        #             1.6: {'label': '1.6', 'style': {'color': 'white'}},
+        #             2: {'label': '2', 'style': {'color': 'white'}},
+        #         },
+        #         id = 'radius',
+        #     ),
+        ], width=2),
+    ]),
+    dbc.Row([
+        html.Div([
+            dbc.Card(
+                dcc.Graph(id='fd-map', figure=blank_fig(500))),
+        ]),
+    ]),
+])
+
+@app.callback(
+    Output("fd-map", "figure"),
+    Input("buffer", 'value'))
+def update_Choropleth(radius):
+    
+    df = pd.read_csv("/Users/jamesswank/Downloads/GC_tobacco_retailers.csv")
+    # print(df)
+    # print(df['x'])
+    # print(df.columns)
+    
+    gdf = gpd.GeoDataFrame(
+        df, geometry=gpd.points_from_xy(df.x, df.y), crs="EPSG:26913" 
+    )
+    # gdf = gpd.GeoDataFrame(
+    #     df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude), crs="EPSG:4326" 
+    # )
+
+    gdf.to_crs(26913)
+
+    schools = pd.read_csv("/Users/jamesswank/Downloads/CDPHE_CDOE_School_Locations_and_District_Office_Locations.csv")
+    print(schools.columns)
+
+
+
+    fig = get_figure(gdf, schools)
+
+
+
+
+
+
+
+
+    return fig
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8050)
+
+
---
+++ figures_utilities.py
@@ -0,0 +1,117 @@
+import plotly.graph_objs as go
+import plotly.express as px
+import numpy as np
+import geopandas as gpd
+
+
+Arap_outline = gpd.read_file('/Users/jamesswank/Python_Projects/Situational_Awareness_v2/us-county-boundaries')
+
+
+
+def get_Choropleth(gdf, schools, marker_opacity, marker_line_width, marker_line_color, fig=None):
+    # print(gd['geometry'])
+    # print(gd.columns)
+    print(gdf['Retailer_Name'])
+    
+    if fig is None:
+        fig = go.Figure(
+        )
+
+    # fig.add_trace(
+    #     go.Choroplethmapbox(
+    #         geojson=eval(df['geometry'].to_json()),
+    #         # geojson=gd,
+    #         locations=df.index,
+    #         z=df['Total'],
+    #         marker_opacity = marker_opacity,
+    #         marker_line_width = marker_line_width,
+    #         marker_line_color = marker_line_color,
+    #         # customdata=gwb["GEOID20"],
+    #         hoverinfo='z',
+    #         colorscale='fall',
+    #         zmax=1000,
+    #         zmin=500
+    #     )
+    # )
+    
+
+    fig.add_trace(
+        go.Scattermapbox(
+            lat=gdf['y'],
+            lon=gdf['x'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=5,
+                color='red'
+            ),
+            customdata=gdf['Retailer_Name'],
+            # hoverinfo='lat',
+            showlegend=False,
+            hovertemplate='<br>'.join([
+            'Store: %{customdata}',
+            ]),
+        )
+    )
+
+    # fig.add_trace(
+    #     go.Scattermapbox(
+    #         lat=schools['LATITUDE'],
+    #         lon=schools['LONGITUDE'],
+    #         mode='markers',
+    #         marker=go.scattermapbox.Marker(
+    #             size=5,
+    #             color='green'
+    #         ),
+    #         # hoverinfo=schools['School_Name'],
+    #         # customdata=schools['School_Name'],
+    #         # hoverinfo='text',
+    #         showlegend=False,
+    #         # hovertemplate='<br>'.join([
+    #         # 'School: %{customdata}',
+    #         # ]),
+    #     )
+    # )
+
+    return fig
+
+# def get_map(df, ):
+
+#     fig = go.Figure()
+  
+
+    
+
+#     return fig
+
+
+def get_figure(gdf, schools):
+
+    # print(df)
+    fig = get_Choropleth(gdf, schools, marker_opacity=1,
+                         marker_line_width=.1, marker_line_color='#6666cc')
+    
+    layer = [
+            {
+                "source": Arap_outline["geometry"].__geo_interface__,
+                "type": "line",
+                "color": "blue"
+            }
+        ]
+    
+    
+    fig.update_layout(mapbox_style="carto-positron", 
+                            mapbox_zoom=10.4,
+                            mapbox_layers=layer,
+                            mapbox_center={"lat": 39.65, "lon": -104.8},
+                            margin={"r":0,"t":0,"l":0,"b":0},
+                            autosize=True,
+                            uirevision='constant'),
+    
+                        
+    
+    # if len(geo_tracts_highlights) != 0:
+    #     fig = get_Choropleth(df, geo_tracts_highlights, marker_opacity=1.0,
+    #                          marker_line_width=3, marker_line_color='aqua', fig=fig)
+    
+
+    return fig
